d3c00ac23954617766eed75fa42b127641012717
Julian <julidj505@gmail.com>
2019-06-20 13:42:55 -0500

    Rest Server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,21 @@
-// ============
-//    Puerto
-// ============
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB
+
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+  urlDB = 'mongodb://cafe-user:Qwerty123@ds133388.mlab.com:33388/heroku_hp26j875'
+}
+process.env.URLDB = urlDB
