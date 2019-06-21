84ac24b25dbfdd0cd4d5bb57738022261af2395c
Julian <julidj505@gmail.com>
2019-06-21 13:00:14 -0500

    Login normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,10 +1,14 @@
 const express = require('express')
+
 const bcrypt = require('bcrypt')
 const _ = require('underscore')
-const app = express()
-const Usuario = require('../models/usuario')
 
-app.get('/usuario', function (req, res) {
+const Usuario = require('../models/usuario')
+const { verificaToken, verificaAdminRole } = require('../middlewares/autenticacion')
+
+const app = express()
+
+app.get('/usuario', verificaToken, (req, res) => {
   let desde = req.query.desde || 0
   desde = Number(desde)
 
@@ -29,7 +33,7 @@ app.get('/usuario', function (req, res) {
   })
 })
 
-app.post('/usuario', function (req, res) {
+app.post('/usuario', [verificaToken, verificaAdminRole], (req, res) => {
   let body = req.body
 
   let usuario = new Usuario({
@@ -53,7 +57,7 @@ app.post('/usuario', function (req, res) {
   })
 })
 
-app.put('/usuario/:id', function (req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdminRole], (req, res) => {
   let id = req.params.id
   let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado'])
   Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
@@ -70,7 +74,7 @@ app.put('/usuario/:id', function (req, res) {
   })
 })
 
-app.delete('/usuario/:id', function (req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdminRole], (req, res) => {
   let id = req.params.id
   let cambiaEstado = {
     estado: false
